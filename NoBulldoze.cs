05338c287af3496cc1467b519f95f838a6a57507
Jonathan Cefalu <jbcefalu@gmail.com>
2022-10-22 22:57:00 -0400

    Formating Refactor

--- NoBulldoze.cs
+++ NoBulldoze.cs
@@ -10,36 +10,30 @@ using UnityEngine.SceneManagement;
 
 namespace NoBulldoze
 {
-    public class NoBulldoze : IUserMod {
-        public string Name {
-            get { return "NoBulldoze mod"; }
-        }
-
-        public string Description {
-            get { return "NoBulldoze mod description"; }
-        }
+    public class NoBulldoze : IUserMod 
+    {
+        public string Name = "NoBulldoze mod";
+        public string Description = "NoBulldoze mod description"; 
     }
 
     public class Loading : LoadingExtensionBase
     {
-        public override void OnLevelLoaded(LoadMode mode) {
-			GameObject go = new GameObject("Test Object");
+        public override void OnLevelLoaded(LoadMode mode)
+	    {
+			var go = new GameObject("Test Object");
 			go.AddComponent<BulldozeBehave>();
 		}
 	}
+
     public class BulldozeBehave : MonoBehaviour {
 
-		void Start(){
-			DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "IT Works!!!  Now what?");
-		}
-
-		void Update(){
-			//
-		}
+		void Start() => DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "IT Works!!!  Now what?");
+		void Update(){ }
 	}
-    public class DestroyMonitor : ThreadingExtensionBase {
-		
-        private readonly BuildingManager _buildingManager;
+
+    public class DestroyMonitor : ThreadingExtensionBase 
+    {
+	    private readonly BuildingManager _buildingManager;
         private readonly SimulationManager _simulationManager;
 
         public DestroyMonitor()
@@ -48,22 +42,32 @@ namespace NoBulldoze
             this._simulationManager = Singleton<SimulationManager>.instance;
 		}
 		
-        public override void OnAfterSimulationTick() {
-            for (var i = (ushort)(_simulationManager.m_currentTickIndex % 1000); i < _buildingManager.m_buildings.m_buffer.Length; i+=1000)
+        public override void OnAfterSimulationTick() 
+        {
+            try
             {
-                if (_buildingManager.m_buildings.m_buffer[i].m_flags == Building.Flags.None)
-                    continue;
+                for (var i = (ushort)(_simulationManager.m_currentTickIndex % 1000); i < _buildingManager.m_buildings.m_buffer.Length; i+=1000)
+                    {
+                        if (_buildingManager.m_buildings.m_buffer[i].m_flags == Building.Flags.None)
+                            continue;
 
-                if ((_buildingManager.m_buildings.m_buffer[i].m_flags & Building.Flags.Abandoned) != Building.Flags.None)
-                {
-	                var building = BuildingManager.instance.m_buildings.m_buffer[i];
-					if (!building.m_flags.IsFlagSet(Building.Flags.Created)) continue;
+                        if ((_buildingManager.m_buildings.m_buffer[i].m_flags & Building.Flags.Abandoned) != Building.Flags.None)
+                        {
+                            var building = BuildingManager.instance.m_buildings.m_buffer[i];
+                            if (!building.m_flags.IsFlagSet(Building.Flags.Created)) 
+                                continue;
 
-					var buildingAi = building.Info.m_buildingAI;
-					if (buildingAi == null) continue;
-					
-					buildingAi.SetHistorical(i, ref BuildingManager.instance.m_buildings.m_buffer[i], true);
-                }
+                            var buildingAi = building.Info.m_buildingAI;
+                            if (buildingAi == null) 
+                                continue;
+                            
+                            buildingAi.SetHistorical(i, ref BuildingManager.instance.m_buildings.m_buffer[i], true);
+                        }
+                    }
+            }
+            catch(exception ex)
+            {
+                DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, $"ex: {ex.Message} Stack Trace: {ex.StackTrace}");
             }
         }
     }
