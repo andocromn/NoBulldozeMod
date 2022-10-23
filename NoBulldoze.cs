88e1dd3bb22d87e7dd0a93c91c0faf6ad0c0e6c0
Jonathan Cefalu <jbcefalu@gmail.com>
2022-10-23 00:13:04 -0400

    Making VS Proj to and using even older version of visual studios

--- NoBulldoze.cs
+++ NoBulldoze.cs
@@ -2,75 +2,90 @@ using ICities;
 using ColossalFramework;
 using ColossalFramework.UI;
 using ColossalFramework.Plugins;
-using System;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using System;
 
 namespace NoBulldoze
 {
-    public class NoBulldoze : IUserMod 
+    public class NoBulldoze : IUserMod
     {
-        public string Name = "NoBulldoze mod";
-        public string Description = "NoBulldoze mod description"; 
+        public string Name
+        {
+            get
+            {
+                return "NoBulldoze mod";
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return "NoBulldoze mod description";
+            }
+        }
     }
 
     public class Loading : LoadingExtensionBase
     {
         public override void OnLevelLoaded(LoadMode mode)
-	    {
-			var go = new GameObject("Test Object");
-			go.AddComponent<BulldozeBehave>();
-		}
-	}
+        {
+            var go = new GameObject("Test Object");
+            go.AddComponent<BulldozeBehave>();
+        }
+    }
 
-    public class BulldozeBehave : MonoBehaviour {
+    public class BulldozeBehave : MonoBehaviour
+    {
 
-		void Start() 
+        void Start()
         {
             DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "IT Works!!!  Now what?");
-        } 
-		void Update(){ }
-	}
+        }
+        void Update()
+        {
 
-    public class DestroyMonitor : ThreadingExtensionBase 
+        }
+    }
+
+    public class DestroyMonitor : ThreadingExtensionBase
     {
-	    private readonly BuildingManager _buildingManager;
+        private readonly BuildingManager _buildingManager;
         private readonly SimulationManager _simulationManager;
 
         public DestroyMonitor()
         {
             this._buildingManager = Singleton<BuildingManager>.instance;
             this._simulationManager = Singleton<SimulationManager>.instance;
-		}
-		
-        public override void OnAfterSimulationTick() 
+        }
+
+        public override void OnAfterSimulationTick()
         {
             try
             {
-                for (var i = (ushort)(_simulationManager.m_currentTickIndex % 1000); i < _buildingManager.m_buildings.m_buffer.Length; i+=1000)
+                for (var i = (ushort)(_simulationManager.m_currentTickIndex % 1000); i < _buildingManager.m_buildings.m_buffer.Length; i += 1000)
+                {
+                    if (_buildingManager.m_buildings.m_buffer[i].m_flags == Building.Flags.None)
+                        continue;
+
+                    if ((_buildingManager.m_buildings.m_buffer[i].m_flags & Building.Flags.Abandoned) != Building.Flags.None)
                     {
-                        if (_buildingManager.m_buildings.m_buffer[i].m_flags == Building.Flags.None)
+                        var building = BuildingManager.instance.m_buildings.m_buffer[i];
+                        if (!building.m_flags.IsFlagSet(Building.Flags.Created))
                             continue;
 
-                        if ((_buildingManager.m_buildings.m_buffer[i].m_flags & Building.Flags.Abandoned) != Building.Flags.None)
-                        {
-                            var building = BuildingManager.instance.m_buildings.m_buffer[i];
-                            if (!building.m_flags.IsFlagSet(Building.Flags.Created)) 
-                                continue;
+                        var buildingAi = building.Info.m_buildingAI;
+                        if (buildingAi == null)
+                            continue;
 
-                            var buildingAi = building.Info.m_buildingAI;
-                            if (buildingAi == null) 
-                                continue;
-                            
-                            buildingAi.SetHistorical(i, ref BuildingManager.instance.m_buildings.m_buffer[i], true);
-                        }
+                        buildingAi.SetHistorical(i, ref BuildingManager.instance.m_buildings.m_buffer[i], true);
                     }
+                }
             }
-            catch(exception ex)
+            catch (Exception ex)
             {
-                DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "ex: {0} Stack Trace: {1}",ex.Message,ex.StackTrace);
+                var errorMessage = string.Format("ex: {0} Stack Trace: {1}", ex.Message, ex.StackTrace);
+                DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, errorMessage);
             }
         }
     }
